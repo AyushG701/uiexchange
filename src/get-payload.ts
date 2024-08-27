35513a8d895e48f21c9a4d678b2f236f1564fbea
Aayush701 <ayushghimire70111@gmail.com>
2024-08-27 11:55:03 +0545

    sign-up verification email added

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,11 +2,22 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
+
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
 
 //   for the email setup also later
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
+});
 
 let cached = (global as any).payload;
 
@@ -33,11 +44,12 @@ export const getPayloadClient = async ({
   }
   if (!cached.promise) {
     cached.promise = payload.init({
-      //   email: {
-      //     transport: transporter,
-      //     fromAddress: "UI@Exchange.com",
-      //     fromName: "UIExchange",
-      //   },
+      email: {
+        transport: transporter,
+        // fromAddress: "UI@Exchange.com", got the error as i dont have regestered and varifiend my own this domain
+        fromAddress: "onboarding@resend.dev",
+        fromName: "UIExchange",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),

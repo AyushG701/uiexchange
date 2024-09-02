7a7343ca065ed8fa1c68f413c1c9b6c463009170
Aayush701 <ayushghimire70111@gmail.com>
2024-09-02 20:48:37 +0545

    Showing product to store completed with small error to fix in ProductReel page

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,8 @@
+import { getPayloadClient } from "@/get-payload";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validators/query-validator";
+import { z } from "zod";
 
 export const appRouter = router({
   // example: public procedure
@@ -8,6 +11,55 @@ export const appRouter = router({
   anyApiRoute: publicProcedure.query(() => {
     return "Hello, world!";
   }),
+
+  // the logic to get the infinite products
+  getInfiniteProducts: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(),
+        query: QueryValidator,
+      }),
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input;
+      const { sort, limit, ...queryOpts } = query;
+
+      const payload = await getPayloadClient();
+
+      const parsedQueryOpts: Record<string, { equals: string }> = {};
+
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
+          equals: value,
+        };
+      });
+
+      const page = cursor || 1;
+
+      const {
+        docs: items,
+        hasNextPage,
+        nextPage,
+      } = await payload.find({
+        collection: "products",
+        where: {
+          approvedForSale: {
+            equals: "approved",
+          },
+          ...parsedQueryOpts,
+        },
+        sort,
+        depth: 1,
+        limit,
+        page,
+      });
+
+      return {
+        items,
+        nextPage: hasNextPage ? nextPage : null,
+      };
+    }),
 });
 
 export type AppRouter = typeof appRouter;

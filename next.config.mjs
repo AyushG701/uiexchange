5891feac8911a37c104976d5b21029cc613b6916
Aayush701 <ayushghimire70111@gmail.com>
2024-09-11 13:08:34 +0545

    completed the slider similar to airbnb for design and having error with writing the persist login for cart

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,16 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
   images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			
-		],
-	},
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
